3fcf896338729476fc152c3fc38bd30a359faaf8
letoproject <dmitry.sytnyk@gmail.com>
2023-11-11 00:26:36 +0200

    Add MongoDB connection

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
-const usersRouter = require('./routes/api/users')
+const DB_HOST =
+  "mongodb+srv://dmitr:2256161Leto@users.rro2nrv.mongodb.net/petProject?retryWrites=true&w=majority";
+const contactsRouter = require("./routes/api/contacts");
+const usersRouter = require("./routes/api/users");
 
-const app = express()
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect"))
+  .catch(e => console.log(e));
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use('/api/contacts', contactsRouter)
-app.use('/api/users', usersRouter)
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  const {status = 500, message = "Server error"} = err
-  res.status(status).json({ message })
-})
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
-module.exports = app
+module.exports = app;
